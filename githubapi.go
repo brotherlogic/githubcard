19e2c115624766854e7f10653c158cc52d7ed2ef
Simon Tucker <brotherlogic@gmail.com>
2023-01-23 08:45:00 -0800

    Bounce

--- githubapi.go
+++ githubapi.go
@@ -232,7 +232,7 @@ func (g *GithubBridge) AddIssue(ctx context.Context, in *pb.Issue) (*pb.Issue, e
 	if in.Uid == 0 {
 		in.Uid = time.Now().UnixNano()
 	}
-	g.CtxLog(ctx, fmt.Sprintf("Adding Issue: %v -> %v/%v (%v)", in.GetTitle(), in.GetService(), r.Number, in))
+	g.CtxLog(ctx, fmt.Sprintf("Adding THE Issue: %v -> %v/%v (%v)", in.GetTitle(), in.GetService(), r.Number, in))
 
 	config.TitleToIssue[in.GetTitle()] = fmt.Sprintf("%v/%v", in.GetService(), in.GetNumber())
 	mapSize.Set(float64(len(config.GetTitleToIssue())))
