cfc0bba34329137caa1affd30b8690c6d204208d
Simon Tucker <brotherlogic@gmail.com>
2023-01-24 09:16:51 -0800

    Dummy

--- client/client.go
+++ client/client.go
@@ -49,7 +49,7 @@ func (c *GHClient) AddIssue(ctx context.Context, in *pb.Issue) (*pb.Issue, error
 			return nil, status.Errorf(c.ErrorCode, "Forced Error")
 		}
 		if c.AddErrorCode != codes.OK {
-			return nil, status.Errorf(c.ErrorCode, "Forced Error")
+			return nil, status.Errorf(c.AddErrorCode, "Forced Error")
 		}
 		c.Issues = append(c.Issues, in)
 		c.lastNumber++
